506b007dcb53bd43508717075a124fb331abaeea
User <user@email.com>
2024-09-11 14:04:47 +0300

    ЛЗ4

--- firstapp/views.py
+++ firstapp/views.py
@@ -6,3 +6,9 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello World! Это мой первый проект на Django!")
+
+def about(request):
+    return HttpResponse("<h2>0 сайте</h2>")
+
+def contact(request):
+    return HttpResponse("<h2>Koнтaкты</h2>")
--- hello/urls.py
+++ hello/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from firstapp import views
 
 urlpatterns = [
-    path('', views.index, name='home'),
-    path('admin/', admin.site.urls),
+    re_path (r'^about/contact/', views.contact),
+    re_path(r'^about', views.about),
+    path ("", views. index)
 ]
