7744a902b23b4ff864d8827e2266a5ebcdf46fa1
Your Name <you@example.com>
2024-09-12 12:59:48 +0300

    ЛЗ7

--- firstapp/views.py
+++ firstapp/views.py
@@ -4,13 +4,16 @@ from django.http import HttpResponse, HttpResponseRedirect, HttpResponsePermanen
 # Create your views here.
 
 
+# def index(request):
+#     header = "Персональные данные"  # обычная переменная     
+#     langs = ["Английский", "Немецкий", "Испанский"]  # массив     
+#     user = {"name": "Максим,", "age": 30}  # словарь     
+#     addr = ("Виноградная", 23, 45)  # кортеж 
+#     data = {"header": header, "langs": langs, "user": user, "address": addr}     
+#     return render(request, "index.html", context=data) 
+
 def index(request):
-    header = "Персональные данные"  # обычная переменная     
-    langs = ["Английский", "Немецкий", "Испанский"]  # массив     
-    user = {"name": "Максим,", "age": 30}  # словарь     
-    addr = ("Виноградная", 23, 45)  # кортеж 
-    data = {"header": header, "langs": langs, "user": user, "address": addr}     
-    return render(request, "index.html", context=data) 
+ return render(request, "firstapp/home.html")
  
 
 def about(request):
