142439c298e3ce1c923a2ee0371d6b76dd7bd52a
m4ydd4y <danmayak@yandex.ru>
2024-09-24 19:09:36 +0300

    ЛЗ9 - Маяков (до конца)

--- firstapp/forms.py
+++ firstapp/forms.py
@@ -1,7 +1,8 @@
 from django import forms
-class UserForm(forms.Form):
-    name = forms.CharField(label="Имя клиента", min_length=3)
-    age = forms.IntegerField(label="Возраст клиента", min_value=1, max_value=100)
-    weight = forms.DecimalField(min_value=3, max_value=200, decimal_places=2)
-    email = forms.EmailField(label="Электронный адрес")
-    reklama = forms.BooleanField(label="Согласны получать рекламу", required=False)
+class UserForm(forms.Form): 
+    name = forms.CharField(label="Имя клиента", 
+                           widget=forms.TextInput(attrs={"class": "myfield"}))     
+    age = forms.IntegerField(label="Возраст клиента", 
+                             widget=forms.NumberInput(attrs={"class": "myfield"})) 
+
+
--- firstapp/views.py
+++ firstapp/views.py
@@ -5,13 +5,13 @@ from django.http import HttpRequest, HttpResponse, HttpResponseRedirect, HttpRes
 # Create your views here.
 
 def index(request):
- userform = UserForm()
- if request.method == "POST":
-    userform = UserForm(request.POST)
- if userform.is_valid():
-    name = userform.cleaned_data["name"]
-    return HttpResponse("<h2>Имя введено коррректно – {0}</h2>".format(name))
- return render(request, "firstapp/index.html", {"form": userform})
+    userform = UserForm()
+    if request.method == "POST":
+        userform = UserForm(request.POST)
+    if userform.is_valid():
+        name = userform.cleaned_data["name"]
+        return HttpResponse("<h2>Имя введено коррректно – {0}</h2>".format(name))
+    return render(request, "firstapp/index.html", {"form": userform})
 
 def details(request):
     return HttpResponsePermanentRedirect("/")
