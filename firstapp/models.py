a03c4055294f551854bb7a597a805e817af1bbcd
m4ydd4y <danmayak@yandex.ru>
2024-09-28 23:57:18 +0300

    ПЗ7 - Маяков - полностью

--- firstapp/models.py
+++ firstapp/models.py
@@ -2,7 +2,17 @@ from django.db import models
 
 # Create your models here.
 
-class Person(models.Model):     
+class Course(models.Model): 
+    name = models.CharField(max_length=30) 
+ 
+class Student(models.Model):     
+    name = models.CharField(max_length=30)     
+    courses = models.ManyToManyField(Course) 
+
+class User(models.Model): 
     name = models.CharField(max_length=20) 
-    age = models.IntegerField()
-    objects = models.Manager() 
+ 
+class Account(models.Model):     
+    login = models.CharField(max_length=20)     
+    password = models.CharField(max_length=20) 
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True) 
--- firstapp/views.py
+++ firstapp/views.py
@@ -1,45 +1,11 @@
 from django.shortcuts import render
 from .forms import UserForm
 from django.http import HttpRequest, HttpResponse, HttpResponseRedirect, HttpResponsePermanentRedirect, HttpResponseNotFound
-from .models import Person
 
 # Create your views here.
 
 def index(request): 
-    people = Person.objects.all()     
-    return render(request, "firstapp/index.html", {"people": people})
-
-def create(request):     
-    if request.method == "POST": 
-        klient = Person() 
-        klient.name = request.POST.get("name")         
-        klient.age = request.POST.get("age")         
-        klient.save() 
-    return HttpResponseRedirect("/")
-
-# изменение данных в БД 
-def edit(request, id):     
-    try: 
-        person = Person.objects.get(id=id)
-        if request.method == "POST":             
-            person.name = request.POST.get("name")             
-            person.age = request.POST.get("age")             
-            person.save()             
-            return HttpResponseRedirect("/")         
-        else: 
-            return render(request, "firstapp/edit.html", {"person": person})     
-    except Person.DoesNotExist: 
-        return HttpResponseNotFound("<h2>Клиент не найден</h2>") 
- 
-# удаление данных из БД 
-def delete(request, id):     
-    try: 
-        person = Person.objects.get(id=id)         
-        person.delete()         
-        return HttpResponseRedirect("/")     
-    except Person.DoesNotExist: 
-        return HttpResponseNotFound("<h2>Клиент не найден</h2>") 
-
+    pass
 
 def details(request):
     return HttpResponsePermanentRedirect("/")
