0a7407e9560d81b0d540f09f85ffd565d2c9bea8
Your Name <you@example.com>
2024-11-26 11:08:59 +0300

    ЛЗ15

--- firstapp/views.py
+++ firstapp/views.py
@@ -4,8 +4,21 @@ from django.http import HttpRequest, HttpResponse, HttpResponseRedirect, HttpRes
 
 # Create your views here.
 
-def index(request): 
-    pass
+def index(request):
+    my_kv = ['I квартал ->', 'II квартал ->', 'III квартал->',
+    'IV квартал->']
+    my_month = ['Январь', 'Февраль', 'Март',
+    'Апрель', 'Май', 'Июнь',
+    'Июль', 'Август', 'Сентябрь',
+    'Октябрь', 'Ноябрь', 'Декабрь']
+    context = {'my_month': my_month, 'my_kv': my_kv}
+    return render(request, "firstapp/index.html", context)
+
+def about(request):
+    return render(request, "firstapp/about.html")
+
+def contact(request):
+    return render(request, "firstapp/contact.html")
 
 def details(request):
     return HttpResponsePermanentRedirect("/")
--- hello/urls.py
+++ hello/urls.py
@@ -19,10 +19,9 @@ from firstapp import views
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path("", views.index),
-    path('about/', TemplateView.as_view(template_name="firstapp/about.html")),
-    path('contact/', TemplateView.as_view(template_name="firstapp/contact.html", 
-                                          extra_context={"work": "Разработка программных продуктов"})),
+    path("", views.index, name="index"),
+    path('about/', views.about, name="about"),
+    path('contact/', views.contact, name="contact"),
     path('details/', views.details),
     path('products/', views.products),   
     path('products/<int:productid>/', views.products),     
