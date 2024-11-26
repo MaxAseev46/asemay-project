83cb85b1804ca95a6a45339bc313b03031cc857a
Your Name <you@example.com>
2024-11-26 12:28:47 +0300

    ЛЗ16 - работа на паре

--- hello/urls.py
+++ hello/urls.py
@@ -26,5 +26,9 @@ urlpatterns = [
     path('products/', views.products),   
     path('products/<int:productid>/', views.products),     
     path('users/', views.users), 
-    path('users/<int:id>/<str:name>/', views.users)
+    path('users/<int:id>/<str:name>/', views.users),
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('my_form/', views.my_form, name='my_form'),
 ]
