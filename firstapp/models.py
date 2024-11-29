6366ad7b77269965e9b6d3ee894094dbb2381243
Your Name <you@example.com>
2024-11-29 10:15:57 +0300

    ЛЗ17 - работа на паре

--- firstapp/models.py
+++ firstapp/models.py
@@ -9,10 +9,17 @@ class Student(models.Model):
     name = models.CharField(max_length=30)     
     courses = models.ManyToManyField(Course) 
 
-class User(models.Model): 
+class Person(models.Model): 
     name = models.CharField(max_length=20) 
+    age = models.IntegerField()     
+    object_person = models.Manager() 
+    DoesNotExist = models.Manager 
  
-class Account(models.Model):     
-    login = models.CharField(max_length=20)     
-    password = models.CharField(max_length=20) 
-    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True) 
+class Company(models.Model): 
+    name = models.CharField(max_length=30) 
+
+ 
+class Product(models.Model): 
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)     
+    name = models.CharField(max_length=30) 
+    price = models.IntegerField() 
